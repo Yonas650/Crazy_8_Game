4338470efe875ea8a829be1d1bc4a0384d044465
Yonas650 <111200835+Yonas650@users.noreply.github.com>
2023-09-21 20:55:28 -0400

    feat: Implement computer turn logic, including Crazy Eights handling

--- bin/game.mjs
+++ bin/game.mjs
@@ -44,3 +44,47 @@ function displayGameState(deck, playerHand, computerHand, discardPile, nextPlay)
     console.log('-'.repeat(windowWidth));
 }
 
+// Function to handle the computer's turn
+function computerTurn(deck, computerHand, discardPile, nextPlay) {
+    // Filtering cards that can be played
+    const playableCards = computerHand.filter(card => matchesAnyProperty(card, nextPlay) || card.rank === '8');
+    //console.log(`test: ${handToString(playableCards)}`)
+    let cardPlayed;
+    console.log(`Computer's hand is: ${handToString(computerHand)}`);
+    if (playableCards.length) {
+        cardPlayed = { ...playableCards[0] };// Clone the object
+        computerHand.splice(computerHand.indexOf(playableCards[0]), 1);// Remove the original card from the computer's hand
+        discardPile.push(cardPlayed);// Add the clone to the discard pile
+    } else {
+        [deck, cardPlayed] = drawUntilPlayable(deck, nextPlay);
+
+        if (cardPlayed) {
+            computerHand.push(...cardPlayed.slice(0, cardPlayed.length - 1));// Add all but last to the computer's hand
+            cardPlayed = { ...cardPlayed[cardPlayed.length - 1] };// Clone the object
+            discardPile.push(cardPlayed);// Add the clone to the discard pile
+        } else {
+            console.log("🤖 Computer couldn't find a playable card even after drawing.");
+            return null;// Return null indicating computer couldn't play
+        }
+    }
+
+    if (cardPlayed.rank === '8') {
+       cardPlayed.suit = SUITS[Math.floor(Math.random() * SUITS.length)];// Modify the clone's suit
+        //nextPlay.suit=SUITS[Math.floor(Math.random() * SUITS.length)];
+        nextPlay = {
+            ...cardPlayed,
+            showSuitOnly: true
+        };
+    }
+    if (cardPlayed.rank !== '8') {
+        nextPlay = {
+            ...cardPlayed,
+            showSuitOnly: false
+        };
+    }
+    console.log(`🤖 Computer played: ${cardPlayed.rank}${cardPlayed.suit}`);
+    question("Press ENTER to continue");
+
+    //return { cardPlayed };
+    return { cardPlayed, nextPlay };
+}
